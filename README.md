4b38d2b95c14601038f06acad25cd96bf6c3f4c4
peremasip <pere.masip@estudiantat.upc.edu>
2021-12-09 16:08:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-n874t7-fyaweb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-n874t7-fyaweb)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
+  product: Product | undefined;
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    //encontrar el producto que corresponde al ID
+    this.product = products.find((product) => product.id == productIdFromRoute);
+  }
+}
