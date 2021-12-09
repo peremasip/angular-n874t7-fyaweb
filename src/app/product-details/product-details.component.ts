9be247c50cb045e221c2b71c29a3a87b2817ff8a
pere.masip <pere.masip@estudiantat.upc.edu>
2021-12-09 17:14:25 +0100

    yessir

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
+
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private cartService: CartService) {}
   product: Product | undefined;
 
   ngOnInit(): void {
@@ -16,5 +19,10 @@ export class ProductDetailsComponent implements OnInit {
     const productIdFromRoute = Number(routeParams.get('productId'));
     //encontrar el producto que corresponde al ID
     this.product = products.find((product) => product.id == productIdFromRoute);
+  }
+
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 }
